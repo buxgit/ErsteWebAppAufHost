5af4d639a8372d97fc445ff817f55f22916f5f12
seb bux <sbucks@outlook.de>
2026-02-17 23:43:12 +0100

    button Gruppe erstellen js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+
+let gameStarted = false;
+
+// erst die Karten erstellen lassen
+cardBuild(3,4);
+
 document.getElementById("startBtn").addEventListener("click", function() {
   document.getElementById("headline").innerText = "CaBuX gestartet";
   this.style.backgroundColor = "green";
+  gameStarted = true;
+  console.log(`gameStarted = ${gameStarted}`)
 });
+
