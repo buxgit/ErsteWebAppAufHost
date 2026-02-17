482d0640c1370c36a893b0befe2f26afda57a0fd
seb bux <sbucks@outlook.de>
2026-02-17 23:07:32 +0100

    button color

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementById("startBtn").addEventListener("click", function() {
   document.getElementById("headline").innerText = "CaBuX gestartet";
+  this.style.backgroundColor = "green";
 });
